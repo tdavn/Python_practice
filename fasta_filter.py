eb8377a8c6761870479cfc476eb0145909be9ed1
Trinh DA <tdamcb@gmail.com>
2020-10-06 17:52:24 +0900

    Updated removing tabs and new lines

--- fasta_filter.py
+++ fasta_filter.py
@@ -6,6 +6,8 @@ import re
 
 # Input sequence
 origin_seq = input('Paste the sequence: ')
-intermediate_seq = origin_seq.replace(' ', '').upper() # remove space. Tabs need checking
-final_seq = re.sub('\d', '', intermediate_seq) # remove digits
+remove_char = [' ', '\t', '\n']
+for chr in remove_char:
+    origin_seq = origin_seq.replace(chr, '').upper() # remove space. Tabs need checking
+final_seq = re.sub('\d', '', origin_seq) # remove digits
 print(final_seq)
