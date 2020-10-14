15032e7d9d283f25fbf06378cccbf7c5188b5a78
Trinh DA <tdamcb@gmail.com>
2020-10-14 09:03:13 -0400

    updated fasta filter and added sgRNA analysis

--- fasta_filter.py
+++ fasta_filter.py
@@ -3,6 +3,8 @@ This code aims to remove space and digits
 from the text, and export a fasta sequence
 '''
 import re
+''' 
+This code fragment is the first trial!
 
 # Input sequence
 origin_seq = input('Paste the sequence: ')
@@ -11,3 +13,10 @@ for chr in remove_char:
     origin_seq = origin_seq.replace(chr, '') # remove space
 final_seq = re.sub('\d', '', origin_seq).upper() # remove digits
 print(final_seq)
+'''
+
+# A new code.
+sample_filter = re.compile(r'\d|\s')
+message = input('Enter a text: ')
+output_mes = sample_filter.sub('', message).upper()
+print(output_mes)
