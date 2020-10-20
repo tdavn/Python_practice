7a21842c9eded997ef53927417612ae4a45369c0
Trinh DA <tdamcb@gmail.com>
2020-10-20 14:58:54 +0900

    add assignmnet w5

---
+++ assignment-w5.py
@@ -0,0 +1,16 @@
+# 4.1 Pizzas
+favorite_pizzas = ['Hot pepper pizza', 'Chicken pizza', 'Maze pizza']
+
+for pizza in favorite_pizzas:
+    print(f'{pizza} is one of my most favorite pizzas.')
+
+print('These ' + str(len(favorite_pizzas)+1) + ' pizzas are so delicieous!')
+
+
+# 4.2 Animals
+pets = ['Hamster', 'Guinea pig', 'Persian cat']
+
+for pet in pets:
+    print(f'{pet} would be a great pet.')
+
+print(f'All of them, {pets[0]}, {pets[1]} and {pets[2]} are really cute!')
